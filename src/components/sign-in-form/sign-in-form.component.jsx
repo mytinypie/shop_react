d8efa90acd53849b83ba914ebea2ad474ca410ae
mytinypie <mytinypie@gmail.com>
2022-05-12 17:33:05 +0300

    created authentication page component alongside sign in form

---
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,85 @@
+import {useState} from "react";
+import './sign-in-form.styles.scss'
+import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component";
+import {
+    createUserDocumentFromAuth,
+    signInAuthUserWithEmailAndPassword,
+    signInWithGooglePopup
+} from "../../utils/firebase/firebase.utils";
+
+const defaultFormFields = {
+    email: '',
+    password: '',
+}
+
+const SignInForm = ()=>{
+    const [formFields, setFormFields] = useState(defaultFormFields);
+    const {email, password} = formFields;
+
+    const resetFormFields =() =>{
+        setFormFields(defaultFormFields);
+    }
+    const signInWithGoogle = async () =>{
+        const {user} = await signInWithGooglePopup();
+         await createUserDocumentFromAuth(user);
+    }
+
+    const handleChange = (event) =>{
+        const {name, value} = event.target;
+        setFormFields({...formFields, [name]: value});
+    }
+    const handleSubmit = async (event) =>{
+        event.preventDefault();
+        try {
+            const response = await signInAuthUserWithEmailAndPassword(email, password);
+            resetFormFields();
+        }catch (e) {
+
+            switch (e.code){
+                case 'auth/wrong-password':
+                    alert('Incorrect password');
+                    break;
+                case 'auth/user-not-found':
+                    alert('No user associated with this email');
+                    break;
+                default:
+                    console.log(e);
+            }
+
+        }
+
+    }
+    return (
+        <div className="sign-up-container">
+            <h2>I already have an account</h2>
+            <span>Sign in with your email and password</span>
+            <form onSubmit={(event)=>{
+                const result = handleSubmit(event);
+            }}>
+                <FormInput
+                    label="Email"
+                    name="email"
+                    type="email"
+                    required
+                    onChange={handleChange}
+                    value={email}
+                />
+                <FormInput
+                    label="Password"
+                    name="password"
+                    type="password"
+                    required
+                    onChange={handleChange}
+                    value={password}
+                />
+                <div className="buttons-container">
+                    <Button type="submit" >Sign In</Button>
+                    <Button type="button" buttonType="google" onClick={signInWithGoogle} >Google Sign In</Button>
+                </div>
+
+            </form>
+        </div>
+    );
+}
+export default SignInForm;
